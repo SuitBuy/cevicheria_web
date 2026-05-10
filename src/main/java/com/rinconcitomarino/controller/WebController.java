de8b1f273c32e3e9c19bf9d8d2c5f9e653b0bfe1
Suit <cabanillasrodaselmer@gmail.com>
2026-05-10 14:56:26 -0500

    update WebController.java and admin.html

--- src/main/java/com/rinconcitomarino/controller/WebController.java
+++ src/main/java/com/rinconcitomarino/controller/WebController.java
@@ -21,6 +21,7 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
+
 import java.util.List;
 
 @Controller
